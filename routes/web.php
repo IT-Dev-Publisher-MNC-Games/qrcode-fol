bba7145ba2f822452ddbc7e104d2326a33cc1bae
gumelarid <galuhprahadi96@gmail.com>
2022-11-02 11:09:50 +0700

    qrcode

---
+++ routes/web.php
@@ -0,0 +1,35 @@
+<?php
+
+use Jenssegers\Agent\Agent;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+Route::get('/link', function (Request $request) {
+
+    $agent = new Agent();
+    $platform = $agent->platform();
+
+    if ($platform == "Android") {
+        return 'open in android';
+    } elseif ($platform == "iphone") {
+        return 'open in iphone';
+    } else {
+        return 'open in browser';
+    }
+})->name('click');
