a8571ec58741491ef9e091b76f52fa69219eab87
IT-Dev-Publisher-MNC-Games <113089846+IT-Dev-Publisher-MNC-Games@users.noreply.github.com>
2022-11-02 15:16:57 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    return 'not found';
+});
+
+Route::get('/fightoflegends', function () {
     $agent = new Agent();
     $platform = $agent->platform();
 
